36b6286981c1a0f4d858e840b9b4b912f43a20c5
AngusGMorrison <contact@angus-morrison.com>
2022-10-28 10:57:49 +0100

    Implement list-backed stack

--- 1_fundamentals/3_bags_queues_stacks/stack/stack.go
+++ 1_fundamentals/3_bags_queues_stacks/stack/stack.go
@@ -1,5 +1,6 @@
 package stack
 
+// SliceStack implements a generic stack backed by a slice.
 type SliceStack[E any] struct {
 	slice []E
 }
@@ -30,4 +31,49 @@ func (s *SliceStack[E]) Each(f func(elem E)) {
 	for _, elem := range s.slice {
 		f(elem)
 	}
+}
+
+// ListStack implements a generic stack backed by a linked list.
+type ListStack[E any] struct {
+	len   int
+	first *node[E]
+}
+
+func NewListStack[E any]() *ListStack[E] {
+	return &ListStack[E]{}
+}
+
+func (ls *ListStack[E]) Len() int {
+	return ls.len
+}
+
+func (ls *ListStack[E]) Push(elem E) {
+	ls.first = &node[E]{
+		data: elem,
+		next: ls.first,
+	}
+	ls.len++
+}
+
+func (ls *ListStack[E]) Pop() (E, bool) {
+	if ls.len == 0 {
+		return *new(E), false
+	}
+
+	elem := ls.first.data
+	ls.first = ls.first.next
+	ls.len--
+
+	return elem, true
+}
+
+func (ls *ListStack[E]) Each(f func(elem E)) {
+	for cur := ls.first; cur != nil; cur = cur.next {
+		f(cur.data)
+	}
+}
+
+type node[E any] struct {
+	data E
+	next *node[E]
 }
--- 1_fundamentals/3_bags_queues_stacks/stack/stack_test.go
+++ 1_fundamentals/3_bags_queues_stacks/stack/stack_test.go
@@ -9,19 +9,16 @@ func Test_SliceStack_Len(t *testing.T) {
 	t.Parallel()
 
 	testCases := []struct {
-		name    string
-		slice   []int
-		wantLen int
+		name  string
+		slice []int
 	}{
 		{
-			name:    "stack is empty",
-			slice:   nil,
-			wantLen: 0,
+			name:  "stack is empty",
+			slice: nil,
 		},
 		{
-			name:    "stack is populated",
-			slice:   []int{1},
-			wantLen: 1,
+			name:  "stack is populated",
+			slice: []int{1},
 		},
 	}
 
@@ -32,8 +29,8 @@ func Test_SliceStack_Len(t *testing.T) {
 			t.Parallel()
 
 			stack := &SliceStack[int]{slice: tc.slice}
-			if gotLen := stack.Len(); gotLen != tc.wantLen {
-				t.Errorf("want len %d, got %d", tc.wantLen, gotLen)
+			if gotLen := stack.Len(); gotLen != len(tc.slice) {
+				t.Errorf("want len %d, got %d", len(tc.slice), gotLen)
 			}
 		})
 	}
@@ -164,6 +161,226 @@ func Test_Stack_Each(t *testing.T) {
 				t.Errorf(
 					"want (*SliceStack).Each to have called the recorder with\n\t%v,\ngot\n\t%v",
 					tc.slice, rec.calledWith,
+				)
+			}
+		})
+	}
+}
+
+func Test_ListStack_Len(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name  string
+		nodes []*node[int]
+	}{
+		{
+			name:  "stack is empty",
+			nodes: nil,
+		},
+		{
+			name: "stack is populated",
+			nodes: []*node[int]{
+				{data: 1},
+				{data: 2},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			for i := 0; i < len(tc.nodes)-1; i++ {
+				tc.nodes[i].next = tc.nodes[i+1]
+			}
+
+			var first *node[int]
+			if len(tc.nodes) > 0 {
+				first = tc.nodes[0]
+			}
+
+			stack := &ListStack[int]{
+				len:   len(tc.nodes),
+				first: first,
+			}
+			if gotLen := stack.Len(); gotLen != len(tc.nodes) {
+				t.Errorf("want len %d, got %d", len(tc.nodes), gotLen)
+			}
+		})
+	}
+}
+
+func Test_ListStack_Push(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name  string
+		stack *ListStack[int]
+		data  []int
+	}{
+		{
+			name: "push single node",
+			stack: &ListStack[int]{
+				len:   0,
+				first: nil,
+			},
+			data: []int{1},
+		},
+		{
+			name: "push multiple nodes",
+			stack: &ListStack[int]{
+				len:   0,
+				first: nil,
+			},
+			data: []int{1, 2},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			initialLen := tc.stack.len
+			for _, elem := range tc.data {
+				tc.stack.Push(elem)
+			}
+
+			wantLen := initialLen + len(tc.data)
+			if tc.stack.len != wantLen {
+				t.Errorf("want len %d, got %d", wantLen, tc.stack.len)
+			}
+
+			for i, cur := 1, tc.stack.first; cur != nil; i, cur = i+1, cur.next {
+				wantData := tc.data[len(tc.data)-i]
+				if cur.data != wantData {
+					t.Errorf(
+						"want node at position %d to have data %d, got %d",
+						i, wantData, cur.data,
+					)
+				}
+			}
+		})
+	}
+}
+
+func Test_ListStack_Pop(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name      string
+		stack     *ListStack[int]
+		wantData  int
+		wantOK    bool
+		wantFirst *node[int]
+		wantLen   int
+	}{
+		{
+			name:      "empty stack",
+			stack:     &ListStack[int]{},
+			wantData:  0,
+			wantOK:    false,
+			wantFirst: nil,
+			wantLen:   0,
+		},
+		{
+			name: "one-node stack",
+			stack: &ListStack[int]{
+				len:   1,
+				first: &node[int]{data: 1},
+			},
+			wantData:  1,
+			wantOK:    true,
+			wantFirst: nil,
+			wantLen:   0,
+		},
+		{
+			name: "multiple-node stack",
+			stack: &ListStack[int]{
+				len: 2,
+				first: &node[int]{
+					data: 1,
+					next: &node[int]{
+						data: 2,
+					},
+				},
+			},
+			wantData: 1,
+			wantOK:   true,
+			wantFirst: &node[int]{
+				data: 2,
+			},
+			wantLen: 1,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			gotData, gotOK := tc.stack.Pop()
+			if gotData != tc.wantData {
+				t.Errorf("want data %d, got %d", tc.wantData, gotData)
+			}
+			if gotOK != tc.wantOK {
+				t.Errorf("want OK %t, got %t", tc.wantOK, gotOK)
+			}
+			if !reflect.DeepEqual(tc.stack.first, tc.wantFirst) {
+				t.Errorf("want first node %+v, got %+v", tc.wantFirst, tc.stack.first)
+			}
+			if tc.stack.len != tc.wantLen {
+				t.Errorf("want stack len %d, got %d", tc.wantLen, tc.stack.len)
+			}
+		})
+	}
+}
+
+func Test_ListStack_Each(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name           string
+		stack          *ListStack[int]
+		wantCalledWith []int
+	}{
+		{
+			name:           "empty stack",
+			stack:          &ListStack[int]{},
+			wantCalledWith: nil,
+		},
+		{
+			name: "populated stack",
+			stack: &ListStack[int]{
+				len: 2,
+				first: &node[int]{
+					data: 1,
+					next: &node[int]{
+						data: 2,
+					},
+				},
+			},
+			wantCalledWith: []int{1, 2},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			rec := &callRecorder{}
+			tc.stack.Each(rec.recordEach)
+			if !reflect.DeepEqual(tc.wantCalledWith, rec.calledWith) {
+				t.Errorf(
+					"want call recorder to be called with\n\t%v,\ngot\n\t%v",
+					tc.wantCalledWith, rec.calledWith,
 				)
 			}
 		})
