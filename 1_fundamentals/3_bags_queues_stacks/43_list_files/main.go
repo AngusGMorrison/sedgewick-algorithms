6bbfed191d2bd995491c86ed1d354e21788560db
AngusGMorrison <contact@angus-morrison.com>
2022-11-03 18:04:20 +0000

    Q43: List Files

---
+++ 1_fundamentals/3_bags_queues_stacks/43_list_files/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/angusgmorrison/sedgewick_algorithms/1_fundamentals/bqs/queue"
+)
+
+func main() {
+	q, err := enqueueFiles("../..")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var builder strings.Builder
+	q.Each(func(elem string) {
+		_, _ = fmt.Fprintf(&builder, "%s\n", elem)
+	})
+	fmt.Println(builder.String())
+}
+
+func enqueueFiles(dir string) (*queue.SliceQueue[string], error) {
+	q := queue.NewSliceQueue[string]()
+	q.Enqueue(dir) // enqueue root directory
+
+	var enqueueDirectory func(path string, depth int) error
+	enqueueDirectory = func(path string, depth int) error {
+		dirEntries, err := os.ReadDir(path)
+		if err != nil {
+			return err
+		}
+		nextDepth := depth + 1
+		for _, dirEntry := range dirEntries {
+			q.Enqueue(fmt.Sprintf("%s%s", strings.Repeat("\t", depth), dirEntry.Name()))
+			if dirEntry.IsDir() {
+				if err := enqueueDirectory(filepath.Join(path, dirEntry.Name()), nextDepth); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	}
+
+	if err := enqueueDirectory(dir, 1); err != nil {
+		return nil, err
+	}
+
+	return q, nil
+}
