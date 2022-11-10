8bba125fd4d0fcbc658601a8d3730110322cedd1
AngusGMorrison <contact@angus-morrison.com>
2022-11-10 14:35:50 +0000

    Q17: Farthest Pair

---
+++ 1_fundamentals/4_analysis_of_algorithms/17_farthest_pair/farthest_pair.go
@@ -0,0 +1,22 @@
+package farthestpair
+
+import "math"
+
+func FarthestPair(a []float64) []float64 {
+	if len(a) < 2 {
+		return nil
+	}
+
+	min := math.Inf(1)
+	max := math.Inf(-1)
+	for _, n := range a {
+		if n < min {
+			min = n
+		}
+		if n > max {
+			max = n
+		}
+	}
+
+	return []float64{min, max}
+}
---
+++ 1_fundamentals/4_analysis_of_algorithms/17_farthest_pair/farthest_pair_test.go
@@ -0,0 +1,49 @@
+package farthestpair
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_FarthestPair(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name     string
+		input    []float64
+		wantPair []float64
+	}{
+		{
+			name:     "slice is empty",
+			input:    nil,
+			wantPair: nil,
+		},
+		{
+			name:     "len(input) == 1",
+			input:    []float64{1},
+			wantPair: nil,
+		},
+		{
+			name:     "len(input) == 2",
+			input:    []float64{2, 1},
+			wantPair: []float64{1, 2},
+		},
+		{
+			name:     "len(input) > 2",
+			input:    []float64{2, 1, 4, 9, 3},
+			wantPair: []float64{1, 9},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			if gotPair := FarthestPair(tc.input); !reflect.DeepEqual(gotPair, tc.wantPair) {
+				t.Errorf("/nwant\n\t%v\ngot\n\t%v", tc.wantPair, gotPair)
+			}
+		})
+	}
+}
