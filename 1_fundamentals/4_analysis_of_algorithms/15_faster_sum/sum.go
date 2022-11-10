c119e0af279d7a71b03f52c660b10d29d22bcaa9
AngusGMorrison <contact@angus-morrison.com>
2022-11-10 11:43:46 +0000

    Q15: Faster Sum

---
+++ 1_fundamentals/4_analysis_of_algorithms/15_faster_sum/sum.go
@@ -0,0 +1,60 @@
+package sum
+
+import "golang.org/x/exp/slices"
+
+// a must be sorted
+func TwoSumLinear(a []int) int {
+	var count int
+	lo := 0
+	hi := len(a) - 1
+	for lo < hi && a[lo] <= 0 && a[hi] >= 0 {
+		if a[lo] < -a[hi] {
+			lo++
+		} else if a[lo] > -a[hi] {
+			hi--
+		} else {
+			// Count the negative and positive elements that may form a valid pair, and multiply
+			// them get the number of pairs that may be generated from these elements.
+			var lowCount, hiCount int
+			for val := a[lo]; lo < hi && a[lo] == val; lo++ {
+				lowCount++
+			}
+			for val := a[hi]; lo <= hi && a[hi] == val; hi-- { // note lo <= hi, since lo may have advanced to hi, but hi has not yet been processed
+				hiCount++
+			}
+
+			count += lowCount * hiCount
+		}
+	}
+
+	return count
+}
+
+func ThreeSumQuadratic(a []int) int {
+	slices.Sort(a) // sorting doesn't affect the asymptotic running time
+
+	var count int
+	for i := 0; i < len(a)-2 && a[i] <= 0; i++ {
+		lo := i + 1
+		hi := len(a) - 1
+		for twoSum := a[i] + a[lo]; lo < hi && twoSum <= 0; twoSum = a[i] + a[lo] {
+			if twoSum < -a[hi] {
+				lo++
+			} else if twoSum > -a[hi] {
+				hi--
+			} else {
+				var lowCount, hiCount int
+				for val := twoSum; lo < hi && a[i]+a[lo] == val; lo++ {
+					lowCount++
+				}
+				for val := a[hi]; lo <= hi && a[hi] == val; hi-- {
+					hiCount++
+				}
+
+				count += lowCount * hiCount
+			}
+		}
+	}
+
+	return count
+}
---
+++ 1_fundamentals/4_analysis_of_algorithms/15_faster_sum/sum_test.go
@@ -0,0 +1,114 @@
+package sum
+
+import "testing"
+
+func Test_TwoSumLinear(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name      string
+		input     []int
+		wantCount int
+	}{
+		{
+			name:      "slice is empty",
+			input:     nil,
+			wantCount: 0,
+		},
+		{
+			name:      "slice contains only negative values",
+			input:     []int{-3, -2, -1, -0},
+			wantCount: 0,
+		},
+		{
+			name:      "slice contains only positive values",
+			input:     []int{0, 1, 2, 3},
+			wantCount: 0,
+		},
+		{
+			name:      "slice contains negative and positive values but no pairs",
+			input:     []int{-3, -1, 0, 2, 4},
+			wantCount: 0,
+		},
+		{
+			name:      "slice contains valid pairs but no duplicates",
+			input:     []int{-3, -2, -1, 0, 1, 2, 3},
+			wantCount: 3,
+		},
+		{
+			name:      "slice contains valid pairs, including an even number of duplicates",
+			input:     []int{-3, -2, -1, 1, 2, 3, 3},
+			wantCount: 4,
+		},
+		{
+			name:      "slice contains valid pairs, including an odd number of duplicates",
+			input:     []int{-3, -2, -1, 1, 1, 2, 3, 3},
+			wantCount: 5,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			if gotCount := TwoSumLinear(tc.input); gotCount != tc.wantCount {
+				t.Errorf("want %d, got %d", tc.wantCount, gotCount)
+			}
+		})
+	}
+}
+
+func Test_ThreeSumQuadratic(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name      string
+		input     []int
+		wantCount int
+	}{
+		{
+			name:      "slice is empty",
+			input:     nil,
+			wantCount: 0,
+		},
+		{
+			name:      "slice contains only negative values",
+			input:     []int{-3, -2, -1, -0},
+			wantCount: 0,
+		},
+		{
+			name:      "slice contains only positive values",
+			input:     []int{0, 1, 2, 3},
+			wantCount: 0,
+		},
+		{
+			name:      "slice contains negative and positive values but no triples",
+			input:     []int{-3, -1, 0, 2, 7},
+			wantCount: 0,
+		},
+		{
+			name:      "slice contains valid triples but no duplicates",
+			input:     []int{-2, -1, 0, 1, 2},
+			wantCount: 2,
+		},
+		{
+			name:      "slice contains valid triples, including duplicates",
+			input:     []int{-2, -2, -1, 0, 1, 2, 2},
+			wantCount: 5,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			if gotCount := ThreeSumQuadratic(tc.input); gotCount != tc.wantCount {
+				t.Errorf("want %d, got %d", tc.wantCount, gotCount)
+			}
+		})
+	}
+}
