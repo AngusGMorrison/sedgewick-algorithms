e2ced1a07de6ab635a9f6382ee40670ab6c5f7ad
AngusGMorrison <contact@angus-morrison.com>
2022-11-16 11:40:40 +0000

    Q27: Queue with two stacks

---
+++ 1_fundamentals/4_analysis_of_algorithms/27_queue_two_stacks/queue_test.go
@@ -0,0 +1,29 @@
+package queue
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_queue(t *testing.T) {
+	t.Parallel()
+
+	t.Run("items are popped in enqueued order", func(t *testing.T) {
+		t.Parallel()
+
+		q := newQueue[int]()
+		input := []int{1, 2, 3}
+		for _, elem := range input {
+			q.enqueue(elem)
+		}
+
+		var output []int
+		for elem, ok := q.dequeue(); ok; elem, ok = q.dequeue() {
+			output = append(output, elem)
+		}
+
+		if !reflect.DeepEqual(input, output) {
+			t.Errorf("want %v, got %v", input, output)
+		}
+	})
+}
