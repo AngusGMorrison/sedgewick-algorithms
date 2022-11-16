0141628ac97666999497d77fbba94a3ccd60fc2d
AngusGMorrison <contact@angus-morrison.com>
2022-11-16 20:37:53 +0000

    Remove unnecessary copies

--- 1_fundamentals/4_analysis_of_algorithms/29_steque_two_stacks/steque.go
+++ 1_fundamentals/4_analysis_of_algorithms/29_steque_two_stacks/steque.go
@@ -33,29 +33,19 @@ func newSteque[E any]() *steque[E] {
 	}
 }
 
-// enqueue pushes the element onto the bottom of the steque, which is the top of the enq stack. If
-// the previous operation was dequeue, the front of the queue is at the top of deq, so all elements
-// in deq must be popped and pushed onto enq before enqueuing the new element.
 func (s *steque[E]) enqueue(elem E) {
-	for priorElem, ok := s.top.pop(); ok; priorElem, ok = s.top.pop() {
-		s.bottom.push(priorElem)
-	}
 	s.bottom.push(elem)
 }
 
-// push pushes the element onto the top of steque, which is the top of the deq stack. If the
-// previous operation was enqueue, the back of the queue is at the top of enq, so all elements in
-// enq must be popped and pushed onto deq before pushing the new element.
 func (s *steque[E]) push(elem E) {
-	for priorElem, ok := s.bottom.pop(); ok; priorElem, ok = s.bottom.pop() {
-		s.top.push(priorElem)
-	}
 	s.top.push(elem)
 }
 
 func (s *steque[E]) pop() (E, bool) {
-	for priorElem, ok := s.bottom.pop(); ok; priorElem, ok = s.bottom.pop() {
-		s.top.push(priorElem)
+	if s.top.len() == 0 {
+		for priorElem, ok := s.bottom.pop(); ok; priorElem, ok = s.bottom.pop() {
+			s.top.push(priorElem)
+		}
 	}
 	return s.top.pop()
 }
