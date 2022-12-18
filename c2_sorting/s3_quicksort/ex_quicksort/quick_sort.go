00969b2545580b607becf9ffdf2bb5e8df41b646
AngusGMorrison <contact@angus-morrison.com>
2022-12-18 18:10:31 +0000

    Quick sort examples

---
+++ c2_sorting/s3_quicksort/ex_quicksort/quick_sort.go
@@ -0,0 +1,36 @@
+package ex_quicksort
+
+import (
+	"math/rand"
+
+	"golang.org/x/exp/constraints"
+)
+
+func QuickSort[S ~[]E, E constraints.Ordered](s S) {
+	quickSort(s, 0, len(s)-1)
+}
+
+func quickSort[S ~[]E, E constraints.Ordered](s S, lo, hi int) {
+	if lo >= hi {
+		return
+	}
+
+	pivot := partition(s, lo, hi)
+	quickSort(s, lo, pivot)
+	quickSort(s, pivot+1, hi)
+}
+
+func partition[S ~[]E, E constraints.Ordered](s S, lo, hi int) int {
+	pivot := lo + rand.Intn(hi-lo+1)
+	s[pivot], s[hi] = s[hi], s[pivot] // move pivot to the end
+
+	for i := lo; i < hi; i++ {
+		if s[i] < s[hi] { // if the current item is less than the pivot
+			s[i], s[lo] = s[lo], s[i] // pile elements smaller than the pivot on the left-hand side of the array
+			lo++                      // advance the left-hand counter so that these smaller elements can no longer be swapped
+		}
+	}
+
+	s[lo], s[hi] = s[hi], s[lo]
+	return lo
+}
---
+++ c2_sorting/s3_quicksort/ex_three_way_partition/three_way_partition.go
@@ -0,0 +1,44 @@
+package ex_three_way_partition
+
+import "math/rand"
+
+func QuickSort(s []int) {
+	quickSort(s, 0, len(s)-1)
+}
+
+func quickSort(s []int, lo, hi int) {
+	if lo >= hi {
+		return
+	}
+
+	// Choose a pivot and move it to the start.
+	pivotIdx := lo + rand.Intn(hi-lo+1)
+	s[lo], s[pivotIdx] = s[pivotIdx], s[lo]
+	pivot := s[lo]
+
+	// Partition the array into three sections: items less than the pivot at the left-hand end,
+	// items equal to the pivot in the middle, and items greater than the pivot at the right-hand
+	// end.
+	lt := lo    // elements less than the pivot are swapped with the less-than pointer
+	i := lo + 1 // points to the current unchecked item
+	gt := hi    // elements greater than the pivot are swapped with the greater-than pointer
+	for i <= gt {
+		if s[i] < pivot {
+			s[i], s[lt] = s[lt], s[i]
+			lt++
+			i++
+		} else if s[i] > pivot {
+			s[i], s[gt] = s[gt], s[i]
+			gt--
+			// i stays the same, since we haven't previously checked the value that we just swapped into s[i] from s[gt]
+		} else {
+			i++ // s[i] == pivot, so keep it in place
+		}
+	}
+
+	// The middle section containing pivot elements is already in sorted order, so we can skip it in
+	// the recursive calls to sort. This is why three-way partitioning is so efficient for slices
+	// containing large numbers of duplicates.
+	quickSort(s, lo, lt-1)
+	quickSort(s, gt+1, hi)
+}
