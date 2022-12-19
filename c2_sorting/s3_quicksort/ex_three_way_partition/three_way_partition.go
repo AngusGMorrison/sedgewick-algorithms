7350b28115eadc9a619bd9cb3df4af2b0042dd43
AngusGMorrison <contact@angus-morrison.com>
2022-12-19 16:12:37 +0000

    Improve comments

--- c2_sorting/s3_quicksort/ex_three_way_partition/three_way_partition.go
+++ c2_sorting/s3_quicksort/ex_three_way_partition/three_way_partition.go
@@ -19,10 +19,12 @@ func quickSort(s []int, lo, hi int) {
 	// Partition the array into three sections: items less than the pivot at the left-hand end,
 	// items equal to the pivot in the middle, and items greater than the pivot at the right-hand
 	// end.
-	lt := lo    // elements less than the pivot are swapped with the less-than pointer
-	i := lo + 1 // points to the current unchecked item
-	gt := hi    // elements greater than the pivot are swapped with the greater-than pointer
-	for i <= gt {
+	// |---------------|--------------|--------------|--------------|
+	// lo (< pivot)    lt (== pivot)  i (unchecked)  gt (> pivot)   hi
+	lt := lo      // elements less than the pivot are swapped with the less-than pointer. lt always points to the pivot
+	i := lo + 1   // points to the current unchecked item
+	gt := hi      // elements greater than the pivot are swapped with the greater-than pointer
+	for i <= gt { // caution: the element at the current value of gt has not yet been checked; we must check it before terminating the loop
 		if s[i] < pivot {
 			s[i], s[lt] = s[lt], s[i]
 			lt++
@@ -39,6 +41,6 @@ func quickSort(s []int, lo, hi int) {
 	// The middle section containing pivot elements is already in sorted order, so we can skip it in
 	// the recursive calls to sort. This is why three-way partitioning is so efficient for slices
 	// containing large numbers of duplicates.
-	quickSort(s, lo, lt-1)
-	quickSort(s, gt+1, hi)
+	quickSort(s, lo, lt-1) // lt points to the first element equal to pivot
+	quickSort(s, gt+1, hi) // gt points to the last element equal to pivot
 }
