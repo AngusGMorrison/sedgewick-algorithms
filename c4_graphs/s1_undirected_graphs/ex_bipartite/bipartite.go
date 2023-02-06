2245f0e6cf714e59668714fc03f92e7cc9d41920
AngusGMorrison <contact@angus-morrison.com>
2023-02-06 20:53:10 +0000

    Refactor struct into func

--- c4_graphs/s1_undirected_graphs/ex_bipartite/bipartite.go
+++ c4_graphs/s1_undirected_graphs/ex_bipartite/bipartite.go
@@ -2,54 +2,37 @@ package ex_bipartite
 
 import "github.com/angusgmorrison/sedgewick_algorithms/c4_graphs/s1_undirected_graphs/ex_graph"
 
-type BipartiteDetector struct {
-	isBipartite bool
-}
+func IsBipartite(g ex_graph.Graph) bool {
+	vertices := g.V()
+	seen := make([]bool, g.V())
+	color := make([]bool, g.V())
+	isBipartite := true
 
-func New(g ex_graph.Graph) *BipartiteDetector {
-	bd := &BipartiteDetector{
-		isBipartite: true,
-	}
-	bd.detect(g)
-	return bd
-}
-
-func (bd *BipartiteDetector) IsBipartite() bool {
-	return bd.isBipartite
-}
-
-func (db *BipartiteDetector) detect(g ex_graph.Graph) {
-	vertices := g.Vertices()
-	seen := make([]bool, vertices)
-	color := make([]bool, vertices)
-
-	var dfs func(v int)
-	dfs = func(v int) {
+	var visit func(v int)
+	visit = func(v int) {
 		seen[v] = true
 		for _, w := range g.Adjacent(v) {
-			if seen[w] {
-				if color[w] == color[v] {
-					db.isBipartite = false
+			if !seen[w] {
+				color[w] = !color[v]
+				visit(w)
+				if !isBipartite {
 					return
 				}
-				continue
-			}
-
-			color[w] = !color[v]
-			dfs(w)
-			// Return early as soon as we determine that g is not bipartite.
-			if !db.isBipartite {
+			} else if color[w] == color[v] {
+				isBipartite = false
 				return
 			}
 		}
 	}
 
-	// DFS each of the components in g.
 	for v := 0; v < vertices; v++ {
-		if seen[v] {
-			continue
+		if !seen[v] {
+			visit(v)
+			if !isBipartite {
+				return false
+			}
 		}
-
-		dfs(v)
 	}
+
+	return true
 }
